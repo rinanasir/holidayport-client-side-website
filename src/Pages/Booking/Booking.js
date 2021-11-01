129eeff3a1cf78cf40460ccef0f81a8ce802a1aa
rinanasir <rinanasir25@gmail.com>
2021-11-01 13:33:16 +0600

    updated booking

--- src/Pages/Booking/Booking.js
+++ src/Pages/Booking/Booking.js
@@ -1,20 +1,42 @@
+import { faDollarSign } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Button } from 'react-bootstrap';
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
+import { Link } from 'react-router-dom';
+import './Booking.css';
 
 const Booking = () => {
-    const { _id } = useParams();
-    console.log(_id)
+    const { id } = useParams();
+    // console.log(_id)
     const [tour, setTour] = useState({});
 
+    const dollarIcon = <FontAwesomeIcon icon={faDollarSign}></FontAwesomeIcon>
+
     useEffect(() => {
-        fetch(`http://localhost:5000/places/${_id}`)
+        fetch(`http://localhost:5000/places/${id}`)
             .then(res => res.json())
             .then(data => setTour(data));
     }, [])
 
     return (
         <div>
-            <h2>booking id: {_id}</h2>
+            <div>
+                <h1 className="text-warning fs-1 fw-bold mt-5">{tour.name}</h1>
+                <div className="d-flex justify-content-evenly align-items-center mx-5">
+                    <div><img
+                        src={tour.image}
+                        className="d-block img-fluid rounded booking-image"
+                        alt="" /></div>
+                    <div className="text-start mx-5">
+                        <p>{tour.about}</p>
+                        <h5 className="text-danger fw-bold">Price: {dollarIcon} {tour.price}</h5>
+                        <Link>
+                            <Button variant="warning" className="fw-bold">Confirm Booking</Button>
+                        </Link>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
